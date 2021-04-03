de6c5e34e52934514aede4c3c793a2ced95a55a1
Sebabatso-M <52962008+Sebabatso-M@users.noreply.github.com>
2021-04-03 16:28:38 +0200

    Updated
    Added menu and overlay animations

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const menu_btn = document.querySelector('.header__menu-btn');
 const overlay = document.querySelector('.overlay');
 const menu = document.querySelector('.menu');
+const body = document.querySelector('body');
 
-menu_btn.addEventListener('click', ()=>{
-    menu_btn.classList.toggle('open');
+const header = document.querySelector('.header');
+
+menu_btn.addEventListener('click', () => {
+    header.classList.toggle('open');
+    overlay.classList.add('fade-out');
+    menu.classList.add('animated');
+    body.classList.toggle('no-scroll');
 }, false);
