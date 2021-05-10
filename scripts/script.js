45f416519638f6d3a2ef84708b2cdcb84dc5dab5
Sebabatso Mashego <sebabatsomashego.dev@gmail.com>
2021-05-11 00:24:27 +0200

    Updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,62 @@
-const menu_btn = document.querySelector('.header__menu-btn');
-const overlay = document.querySelector('.overlay');
-const menu = document.querySelector('.menu');
+const menu_btn = document.querySelector(".header__menu-btn");
+const overlay = document.querySelector(".overlay");
+const menu = document.querySelector(".menu");
 const html = document.querySelector("html");
-const header = document.querySelector('.header');
-const nav_dropdowns = document.querySelectorAll('.navigation__dropdown');
+const header = document.querySelector(".header");
+const nav_dropdowns = document.querySelectorAll(".navigation__dropdown");
 
 // open/close menu
-menu_btn.addEventListener('click', () => {
+menu_btn.addEventListener(
+    "click",
+    () => {
+        header.classList.toggle("--is-open");
+        overlay.classList.add("fade-out");
+        menu.classList.add("animated");
+        html.classList.toggle("no-scroll");
+        close_dropdowns();
+    },
+    false
+);
 
-    header.classList.toggle('--is-open');
-    overlay.classList.add('fade-out');
-    menu.classList.add('animated');
-    html.classList.toggle('no-scroll');
-    close_dropdowns();
-
-}, false);
-
-nav_dropdowns.forEach(element => {
-    element.addEventListener('click', collapse, false);
-})
+nav_dropdowns.forEach((element) => {
+    element.addEventListener("click", collapse, false);
+});
 
 // open dropdown
 function collapse() {
-
     const parent = this.parentElement;
-    const title = this.children[0]
+    const title = this.children[0];
     const arrow = this.children[1].children[1];
-    const max_height = parent.scrollHeight + 'px';
+    const max_height = parent.scrollHeight + "px";
 
     close_dropdowns(this);
 
-    // if open
-    if (window.getComputedStyle(parent).maxHeight == max_height) {
-        decrease_height(parent);
-    } else {
-        increase_height(parent);
+    // if open and website is in mobile view
+    if (window.getComputedStyle(header).display == "block") {
+        if (window.getComputedStyle(parent).maxHeight == max_height) {
+            decrease_height(parent);
+        } else {
+            increase_height(parent);
+        }
     }
-    title.classList.toggle('active');
-    arrow.classList.toggle('opened');
+    title.classList.toggle("active");
+    arrow.classList.toggle("opened");
 }
 
-// increase height of dropdown 
+// increase height of dropdown
 function increase_height(element) {
-    element.style.maxHeight = element.scrollHeight + 'px';
+    element.style.maxHeight = element.scrollHeight + "px";
 }
 
-// decrease height of dropdown 
+// decrease height of dropdown
 function decrease_height(element) {
-    element.style.maxHeight = '36.8px';
+    element.style.maxHeight = "36.8px";
 }
 
 // closes other open dropdowns
 // not the current active dropdown
 function close_dropdowns(ele = null) {
-
-    nav_dropdowns.forEach(element => {
-
+    nav_dropdowns.forEach((element) => {
         if (ele !== element) {
             const parent = element.parentElement;
             const title = element.children[0];
@@ -63,8 +64,8 @@ function close_dropdowns(ele = null) {
 
             decrease_height(parent);
 
-            title.classList.remove('active');
-            arrow.classList.remove('opened');
+            title.classList.remove("active");
+            arrow.classList.remove("opened");
         }
-    })
+    });
 }
